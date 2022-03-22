89ef4aea2ac287013cd380750f0c631d4b988ee6
Nkuansambo Justem Dombaxe <justindombaxe@hotmail.com>
2022-03-22 14:07:32 +0100

    add

--- mobile.php
+++ mobile.php
@@ -1,23 +1,12 @@
 
       <section class="element-animate">
              <div class="container d-sm-block d-md-none" style="padding-top:11rem">
-               <ul class="dom-mobile">
-                <li class="mr-1">
-                    <a class="btn btn-light btn-sm search-dom" href="#" data-toggle="modal" data-target="#sitemodal"><i class="fa fa-search"></i></a>
-                  </li>
-                  <li class="mr-1">
-                          <a class="btn btn-primary btn-sm fb" href="https://www.facebook.com/servicodemigracaoestrangeiros"><i class="fa fa-facebook"></i></a>
-                  </li>
-                  <li class="mr-1">
-                        <a class="btn btn-danger btn-sm youtb" href="#"><i class="fa fa-youtube"></i></a>
-                  </li>
-                  <li>
-                    <?php
+             <div> <a class="btn btn-light btn-sm search-dom" href="#" data-toggle="modal" data-target="#sitemodal"><i class="fa fa-search"></i></a></div>
+                <div><a class="btn btn-primary btn-sm fb" href="https://www.facebook.com/servicodemigracaoestrangeiros"><i class="fa fa-facebook"></i></a></div>
+                <div><a class="btn btn-danger btn-sm youtb" href="#"><i class="fa fa-youtube"></i></a></div>
+                <div><?php
                       dynamic_sidebar('Lingua');
-                    ?> 
-                  </li>  
-                              
-                </ul>
+                    ?> </div>
                 <div class="row">
                         <div class="col-sm-6 col-md-4">
                             <a target="_blank"  href="//www.smevisa.gov.ao/">
