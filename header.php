7f199b9e3d8cb6211f54d3e6884581c2c61848ee
Nkuansambo Justem Dombaxe <justindombaxe@hotmail.com>
2022-03-24 08:14:24 +0100

    add

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
   </head>
   <body>
     <header class="fixed-top">
-        <nav class="navbar navbar-expand-lg  bg-white">
+        <nav class="navbar navbar-expand-lg max-headera">
             <a class="navbar-brand col-9 logo d-md-block d-none max-headera" href="#">
                     <img src="<?php bloginfo('template_url'); ?>/assets/SME GOV.ao MAUSC.png" width="600px" height="90px">                               
             </a>
